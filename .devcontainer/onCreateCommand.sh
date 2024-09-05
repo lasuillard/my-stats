5b5fd0016e8111a649826fb7583acc64ac624aaf
Yuchan Lee <lasuillard@gmail.com>
2024-09-05 12:31:19 +0000

    chore(devcontainer): Install prom-write

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -6,6 +6,10 @@ ARCH="$(dpkg --print-architecture)"
 curl -sS https://apt.grafana.com/gpg.key | sudo gpg --dearmor --output /usr/share/keyrings/grafana.gpg \
   && echo "deb [signed-by=/usr/share/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
+# prom-write
+sudo curl -L -o /usr/local/bin/prom-write https://github.com/theduke/prom-write/releases/download/v0.2.1/prom-write-linux-x86 \
+  && sudo chmod +x /usr/local/bin/prom-write
+
 sudo apt-get update && sudo apt-get install -y \
   alloy \
   bash-completion
